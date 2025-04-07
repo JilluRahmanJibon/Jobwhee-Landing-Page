027793fd7898def3018c4bf90788166f5b16049f
codeWithThunder <atikulla345@gmail.com>
2025-04-07 12:48:55 +0600

    update footer

--- src/components/stepsofsuccess.js
+++ src/components/stepsofsuccess.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 function stepsofsuccess() {
@@ -8,7 +9,7 @@ function stepsofsuccess() {
   ];
   return (
     <div className="w-full">
-      <div className="[@media(min-width:1210px)]:w-[1200px] w-[100%] mx-auto">
+      <div className="[@media(min-width:1450px)]:!w-[1440px] [@media(min-width:1210px)]:w-[1200px] w-full mx-auto">
         <div className="sm:w-[17.7rem] w-[11.9rem] relative mx-auto flex justify-center items-center text-center">
           <h1 className="text-[#000000] sm:text-[30px] text-[20px] font-[800]">
             STEPS OF SUCCESS
@@ -20,16 +21,26 @@ function stepsofsuccess() {
             <div className="sm:w-[124px] w-[92px] sm:h-[5px] h-[2px] rounded-[25px] bg-[#C0D724]"></div>
           </div>
         </div>
-        <div className="w-[250px] h-[46px] my-[2rem] flex items-center bg-white rounded-[30px]">
-          <button className="w-[50%] h-full text-[13px] font-[500] text-[#18470D] bg-[#ffffff] rounded-[30px]">
+        <div className="w-[297px] h-[56px] mt-[3rem] mb-[1.5rem] flex items-center bg-white rounded-[30px]">
+          <button className="w-[50%] h-full text-[18px] font-[500] text-[#18470D] bg-[#ffffff] rounded-[30px]">
             Client
           </button>
-          <button className="w-[50%] h-full text-[13px] font-[500] text-[#18470D] bg-[#CBEC5E] border-[2px] border-[#ffffff] rounded-[30px]">
+          <button className="w-[50%] h-full text-[18px] font-[500] text-[#18470D] bg-[#CBEC5E] border-[3px] border-[#ffffff] rounded-[30px]">
             Talent
           </button>
         </div>
-        <div className="w-[276px] h-[384px] rounded-[25px] bg-white">
-
+        <div className="w-[275px] h-[384px] rounded-[30px] bg-white relative">
+          {/* <div className="absolute left-[1rem] top-[1rem] w-[50px] h-[50px] rounded-[13px] bg-[#000]" style={{"clip-path": "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)"}}></div> */}
+          <div className="">
+            <Image
+        fill
+              src="/stepofsuccess/1.png"
+              alt="Logo"
+              sizes="185px,"
+              className="cursor-pointer"
+            />
+            <img src="/stepofsuccess/1.png" className="w-[185px]" />
+          </div>
         </div>
       </div>
     </div>
